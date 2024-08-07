250ca0032d810018ea1a76c1519f89e7dfe6ecdd
Lacevedo <30413819+LAcevedo01@users.noreply.github.com>
2024-08-06 21:57:27 -0400

    Add files via upload

---
+++ Assets/BulletKill.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletKill : MonoBehaviour
+{
+    public int damage;
+    public EnemyHealth Ehealth;
+
+   
+    
+}
